9dbcb7584b740f19454260d044f522b3e90e18bc
Joeypiewow <joey121345@naver.com>
2024-02-29 17:37:03 +0900

    2024-02-29 final

--- WebMarket/src/main/java/dto/Product.java
+++ WebMarket/src/main/java/dto/Product.java
@@ -13,6 +13,7 @@ public class Product implements Serializable {
 	private long unitsInStock;
 	private String condition;
 	private String filename;
+	private int quantity;
 
 	public Product() {
 		super();
@@ -95,6 +96,12 @@ public class Product implements Serializable {
 
 	public void setFilename(String filename) {
 		this.filename = filename;
+	} 
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 	
 }
---
+++ WebMarket/src/main/webapp/removeCart.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="dto.Product" %>
+<%@ page import="dao.ProductRepository" %>
+
+<%
+	
+%>
